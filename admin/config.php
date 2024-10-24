30d68df5fc217c9e787f438eeddaa6f511d656e8
Andrey <108361411+WorstC0der@users.noreply.github.com>
2024-10-24 19:57:10 +0300

    После установки OpenCart

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://smartcool.local/admin/');
+define('HTTP_CATALOG', 'https://smartcool.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://smartcool.local/admin/');
+define('HTTPS_CATALOG', 'https://smartcool.local/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/home/SmartCool.local/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/home/SmartCool.local/system/');
+define('DIR_IMAGE', 'C:/OSPanel/home/SmartCool.local/image/');
+define('DIR_STORAGE', 'C:/OSPanel/home/SmartCoolStorage/');
+define('DIR_CATALOG', 'C:/OSPanel/home/SmartCool.local/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'MySQL-8.0');
+define('DB_USERNAME', 'smart');
+define('DB_PASSWORD', 'smart');
+define('DB_DATABASE', 'SmartCool');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
